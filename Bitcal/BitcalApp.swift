241ad4fc5a7b6dac421448ee1723df613d5672ec
David Akhmedbayev <david.akhmedbayev@icloud.com>
2025-05-20 20:07:31 -0400

    Initial Commit

---
+++ Bitcal/BitcalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcalApp.swift
+//  Bitcal
+//
+//  Created by David Akhmdbayev on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BitcalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
